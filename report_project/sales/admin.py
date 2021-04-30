96d57b75e845fde343e1bfbd378f1ee798d93c17
sunny <sunnysacademy0@gmail.com>
2021-04-30 23:51:15 +0530

    Sales models created

--- report_project/sales/admin.py
+++ report_project/sales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Position, Sale, CSV
+
+admin.site.register(Position)
+admin.site.register(Sale)
+admin.site.register(CSV)
