00f78e346031b4c42ce3518cb80beea6ee0e8d96
sunny <sunnysacademy0@gmail.com>
2021-05-09 00:18:24 +0530

    sales updated

--- report_project/sales/views.py
+++ report_project/sales/views.py
@@ -10,6 +10,7 @@ from .utils import get_customer_from_id, get_salesman_from_id
 def home_view(request):
     sales_df = None
     positions_df = None
+    merged_df = None
 
 
     form = SalesSearchForm(request.POST or None)
@@ -26,7 +27,8 @@ def home_view(request):
             sales_df= pd.DataFrame(sale_qs.values())
             sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
             sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
-            sales_df.rename({'customer_id':'customer', 'salesman_id':'salesman'}, axis=1, inplace=True)
+            sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            sales_df.rename({'customer_id':'customer', 'salesman_id':'salesman', 'id':'sales_id'}, axis=1, inplace=True)
             positions_data = []
             for sale in sale_qs:
                 for pos in sale.get_positions():
@@ -39,13 +41,16 @@ def home_view(request):
 
                     }
                     positions_data.append(obj)
+
             positions_df = pd.DataFrame(positions_data)
-            print('positions_df')
-            print(positions_df)
+            merged_df = pd.merge(sales_df, positions_df, on='sales_id')
+            # print('positions_df')
+            # print(positions_df)
 
 
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
+            merged_df = merged_df.to_html()
             
         else:
             print('no data')
@@ -56,6 +61,7 @@ def home_view(request):
         'form':form,
         'sales_df':sales_df,
         'positions_df':positions_df,
+        'merged_df':merged_df,
         
     }
     return render(request, 'sales/home.html', context)
