d005df8ca93e7468ba26b0756c097aadf955d89d
sunny <sunnysacademy0@gmail.com>
2021-05-12 23:04:49 +0530

    sales popup added

--- about.py
+++ about.py
@@ -13,3 +13,5 @@ step - inside sales application created utils.py , signals.py and apps.py update
 step - reports application models.py and admin.py added
 
 step - sale application views.py and urls.py and  sales/templates/main.html, home.html, detail.html  file updates
+
+step - popup added in sales/home.html
